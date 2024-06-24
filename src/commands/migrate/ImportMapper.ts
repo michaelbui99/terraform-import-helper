4c1f5653ddb7c6fb5602c8e9aabfd6e978f42cca
Michael Bui <michaelbui99@hotmail.com>
2024-06-24 20:26:04 +0200

    fix moduleName resolution

--- src/commands/migrate/ImportMapper.ts
+++ src/commands/migrate/ImportMapper.ts
@@ -4,6 +4,7 @@ export interface IImportMapper {
     handles(resourceType: string): boolean;
     generateImports(
         env: "prod" | "prod-test" | "test",
+        project: string,
         resouce: TFResource
     ): string;
 }
@@ -15,12 +16,17 @@ export class AzureAdApplicationImportMapper implements IImportMapper {
 
     generateImports(
         env: "prod" | "prod-test" | "test",
+        project: string,
         resource: TFResource
     ): string {
         let output = "";
 
         for (let instance of resource.instances) {
-            const moduleName = instance.index_key.replaceAll("-", "_");
+            const moduleName = instance.index_key
+                ? instance.index_key.replaceAll("-", "_")
+                : instance.attributes["display_name"].split(
+                      `${project}-${env}-`
+                  )[1];
             const to = `module.${env}.module.${moduleName}.${resource.type}.${resource.name}`;
             const id = instance.attributes["id"];
 
